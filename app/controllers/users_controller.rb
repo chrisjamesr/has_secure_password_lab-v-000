5bca0ec87c81d512b4469a62aa2c88ca22465c24
chrisjamesr <chrisjames.r@gmail.com>
2017-12-31 20:52:20 -0500

    add before action filter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,17 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
+  before_action :current_user, only: [:new]
+  
+  def new    
   end
 
   def create
     @user = User.new(user_params)
-    
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      redirect_to new_user_path
+    end
   end 
 
 
